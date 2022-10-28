ecd9702eb945d82521552b5998a1ee4d544ae55f
Eric Zhao <eric.zhao@ocado.com>
2022-10-27 22:07:46 -0400

    Timer attempt using Timer from threading

--- routine/automated_routine.py
+++ routine/automated_routine.py
@@ -1,9 +1,15 @@
+from multiprocessing import BoundedSemaphore
 import time
+from threading import Timer
 from pymodbus.client import ModbusTcpClient
 
 PROTOS_X_HOST = '192.168.1.142'
 PROX_1_ADDR = 0
 PROX_2_ADDR = 1
+
+REORIENT_TIMEOUT = 1.0
+
+process_sema = BoundedSemaphore(1)
 
 protos_x = ModbusTcpClient(PROTOS_X_HOST)
 
@@ -19,6 +25,13 @@ def dummy_arm_routine(location):
 def stop_long_conveyor():
     pass
 
+def reset_reorient():
+    time.sleep(1)
+    print('Running conveyeor backwards')
+    time.sleep(1)
+    t = Timer(REORIENT_TIMEOUT, reset_reorient)
+    t.start()
+
 def routine():
     # Create a graphic that will help with this
     location = input('Pick Location: ')
@@ -28,25 +41,29 @@ def routine():
     speed = input('Speed: ')
     cycles = int(input('Number of cycles: '))
 
-    while cycles > 0:
-        timer_start = time.time()
+    t = Timer(1.0, reset_reorient)
+    t.start()
 
+    while cycles > 0:
         # Requires prox sensors to be wired to consecutive addresses
         data = protos_x.read_discrete_inputs(PROX_1_ADDR, 2)
         prox_1_sense = data.bits[0]
         prox_2_sense = data.bits[1]
 
         if(prox_1_sense and prox_2_sense):
+            t.cancel()
             print("Both sensors activated.")
             dummy_arm_routine(location)
             cycles -= 1
+            t = Timer(1.0, reset_reorient)
+            t.start()
         elif(prox_1_sense):
             print("Only sensor 1 activated.")
         elif(prox_2_sense):
             print("Only sensor 2 activated")
         else:
             print("Nothing detected.")
-
+    t.cancel()
     print("Automated routine finished.")
 
 if __name__ == '__main__':
