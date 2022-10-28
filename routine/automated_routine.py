0f5b803863a246669a9bd43251f658612358d7c7
Eric Zhao <eric.zhao@ocado.com>
2022-10-27 23:18:07 -0400

    Finalized concept of using the threading timer for 'watchdog timer'.

--- routine/automated_routine.py
+++ routine/automated_routine.py
@@ -1,4 +1,3 @@
-from multiprocessing import BoundedSemaphore
 import time
 from threading import Timer
 from pymodbus.client import ModbusTcpClient
@@ -9,7 +8,7 @@ PROX_2_ADDR = 1
 
 REORIENT_TIMEOUT = 1.0
 
-process_sema = BoundedSemaphore(1)
+routine_state = 0
 
 protos_x = ModbusTcpClient(PROTOS_X_HOST)
 
@@ -26,9 +25,12 @@ def stop_long_conveyor():
     pass
 
 def reset_reorient():
+    global routine_state
+    routine_state = 1
     time.sleep(1)
     print('Running conveyeor backwards')
     time.sleep(1)
+    routine_state = 0
     t = Timer(REORIENT_TIMEOUT, reset_reorient)
     t.start()
 
@@ -41,28 +43,26 @@ def routine():
     speed = input('Speed: ')
     cycles = int(input('Number of cycles: '))
 
-    t = Timer(1.0, reset_reorient)
+    t = Timer(REORIENT_TIMEOUT, reset_reorient)
     t.start()
 
     while cycles > 0:
         # Requires prox sensors to be wired to consecutive addresses
-        data = protos_x.read_discrete_inputs(PROX_1_ADDR, 2)
-        prox_1_sense = data.bits[0]
-        prox_2_sense = data.bits[1]
+        if (not routine_state):
+            data = protos_x.read_discrete_inputs(PROX_1_ADDR, 2)
+            prox_1_sense = data.bits[0]
+            prox_2_sense = data.bits[1]
 
-        if(prox_1_sense and prox_2_sense):
-            t.cancel()
-            print("Both sensors activated.")
-            dummy_arm_routine(location)
-            cycles -= 1
-            t = Timer(1.0, reset_reorient)
-            t.start()
-        elif(prox_1_sense):
-            print("Only sensor 1 activated.")
-        elif(prox_2_sense):
-            print("Only sensor 2 activated")
-        else:
-            print("Nothing detected.")
+            if(prox_1_sense and prox_2_sense):
+                print("Both sensors activated.")
+                dummy_arm_routine(location)
+                cycles -= 1
+            elif(prox_1_sense):
+                print("Only sensor 1 activated.")
+            elif(prox_2_sense):
+                print("Only sensor 2 activated")
+            else:
+                print("Nothing detected.")
     t.cancel()
     print("Automated routine finished.")
 
