12f13c8841b46b4b75c90a7490665f370e7c4ff0
Aathithan Chandrabalan <aathithan.chandrabalan@ocado.com>
2022-12-19 00:58:19 -0500

    AC - GUI Re-design

--- src/main.py
+++ src/main.py
@@ -1,65 +1,88 @@
+import sys
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
-import sys
-import time
-
-class RotatableContainer(QGraphicsView):
-    def __init__(self, widget: QWidget, rotation: float):
-        super(QGraphicsView, self).__init__()
-
-        scene = QGraphicsScene(self)
-        self.setScene(scene)
-
-        self.proxy = QGraphicsProxyWidget()
-        self.proxy.setWidget(widget)
-        self.proxy.setTransformOriginPoint(self.proxy.boundingRect().center())
-        self.proxy.setRotation(rotation)
-        scene.addItem(self.proxy)
-
-    def rotate(self, rotation: float):
-        self.proxy.setRotation(rotation)
-
-
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
         self.setWindowTitle("Induct Testbench Control  (v1.0.0-alpha)")
-        self.setGeometry(0, 0, 1920, 1080)  #posX, posY, w, h
+        self.setGeometry(0, 0, 1920, 1080)
 
-        # Widget you want to rotate
-        label = QLabel("Stack Overflow", alignment=Qt.AlignCenter)
-        # Container you place the widget in
-        container = RotatableContainer(widget=label, rotation=0)
+        # Waypoint Bank
+        
+        waypoint_tree = QRadioButton("RadioButton 1")
 
-        # Create slider and connect to the rotate method in the container
-        slider = QSlider(minimum=45, maximum=359, orientation=Qt.Horizontal)
-        slider.valueChanged.connect(container.rotate)
-        container3 = RotatableContainer(widget=slider, rotation=45)
+        # Bank Controls
+        waypoint_controls = QRadioButton("Import")
 
-        label_text = QLabel("{}°".format(slider.value()), alignment=Qt.AlignCenter)
-        slider.valueChanged.connect(lambda value: label_text.setText("{}°".format(slider.value())))
-        slider.setValue(45)
+        # Dynamic Controls
+        dynamic_controls_g = QGroupBox("Dynamic Controls")
+        x_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
+        y_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
+        z_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
 
-        label2 = QLabel("Jordan", alignment=Qt.AlignCenter)
-        container2 = RotatableContainer(widget=label2, rotation=45)
+        # Static Controls
+        static_controls_g = QGroupBox("Static Controls")
+        xyz_pos = QRadioButton("Static Controls")
 
-        # Display the widgets
-        w = QWidget()
-        lay = QVBoxLayout(w)
-        lay.addWidget(container)
-        lay.addWidget(container2)
-        lay.addWidget(container3)
-        lay.addWidget(slider)
-        lay.addWidget(label_text)
-        w.resize(1920, 1080)
+        # Conveyor Controls
+        conveyor_controls_g = QGroupBox("Conveyor Controls")
+        stop_conveyor = QRadioButton("Conveyor_controls")
 
-        self.setCentralWidget(w)
-        self.show()
+        # Joint Limits
+        joint_limits = QGroupBox("Joint Limits")
+
+        # Logo
+        placeholder = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
 
 
-app = QApplication([])  # Holds the event loop
-window = MainWindow()
-app.exec_()
+
+        # layout 
+        top_row_g = QGroupBox("Top Row")
+        
+
+        dynamic_controls = QHBoxLayout()
+        dynamic_controls.addWidget(x_slider)
+        dynamic_controls_g.setLayout(dynamic_controls)
+
+        static_controls = QHBoxLayout()
+        static_controls.addWidget(xyz_pos)
+        static_controls_g.setLayout(static_controls)
+        
+
+        conveyor_controls = QHBoxLayout()
+        conveyor_controls.addWidget(stop_conveyor)
+        conveyor_controls_g.setLayout(conveyor_controls)
+
+        top_row = QHBoxLayout()
+        top_row.addWidget(waypoint_tree)
+        top_row.addWidget(waypoint_controls)
+        top_row_g.setLayout(top_row)
+
+
+        bottom_row = QHBoxLayout()
+        bottom_row.addWidget(dynamic_controls_g)
+        bottom_row.addWidget(static_controls_g)
+        bottom_row.addWidget(conveyor_controls_g)
+
+        lhs = QVBoxLayout()
+        lhs.addWidget(top_row_g)
+        lhs.addLayout(bottom_row)
+
+        rhs = QVBoxLayout()
+        rhs.addWidget(z_slider)
+        rhs.addWidget(placeholder)
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(lhs)
+        main_layout.addLayout(rhs)
+
+        self.setLayout(main_layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # event loop
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
