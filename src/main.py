a9b5299f4adaecf40baec044a8459d4c187cb78b
Aathithan Chandrabalan <aathithan.chandrabalan@ocado.com>
2022-12-19 13:42:46 -0500

    AC - Static Controls

--- src/main.py
+++ src/main.py
@@ -7,8 +7,20 @@ class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("Induct Testbench Control  (v1.0.0-alpha)")
-        self.setGeometry(0, 0, 1920, 1080)
+        self.setGeometry(0, 0, 1160, 640)
 
+        # design 
+        title=QFont()
+        title.setBold(True)
+        #title.setPixelSize()
+
+        unbold=QFont()
+        unbold.setBold(False)
+        #title.setPixelSize()
+
+
+        
+        
         # Waypoint Bank
         
         waypoint_tree = QRadioButton("RadioButton 1")
@@ -18,26 +30,72 @@ class MainWindow(QWidget):
 
         # Dynamic Controls
         dynamic_controls = QGroupBox("Dynamic Controls")
-        accel_label = QLabel("arm acceleration:")
-        accel_value = QLabel("344")
+        dynamic_controls.setFont(title)
+    
         accel_dial = QDial(self)
-        x_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
-        y_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
-        z_slider = QSlider(minimum=45, maximum=80, orientation=Qt.Horizontal)
+        accel_dial.setMinimum(0)
+        accel_dial.setMaximum(100)
+        accel_dial.setValue(75)
+    
+        accel_label = QLabel("acceleration limiter:")
+        accel_label.setFont(unbold)
+        accel_value = QLabel("{}%".format(accel_dial.value()))
+        accel_dial.valueChanged.connect(lambda value: accel_value.setText("{}%".format(accel_dial.value())))
+        
+        x_slider = QSlider(minimum=0, maximum=80, orientation=Qt.Horizontal)
+        y_slider = QSlider(minimum=0, maximum=80, orientation=Qt.Horizontal)
+        z_slider = QSlider(minimum=0, maximum=80, orientation=Qt.Horizontal)
         x_label = QLabel("x:")
         y_label = QLabel("y:")
         z_label = QLabel("z:")
-        x_value = QLabel("x:")
-        y_value = QLabel("y:")
-        z_value = QLabel("z:")
-        submit_name = QLineEdit('Waypoint name')
+
+        x_value = QLabel("{}".format(x_slider.value()))
+        x_slider.valueChanged.connect(lambda value: x_value.setText("{}".format(x_slider.value())))
+        x_value.setFont(title)
+        x_value.setStyleSheet('color: deeppink')
+        y_value = QLabel("{}".format(y_slider.value()))
+        y_slider.valueChanged.connect(lambda value: y_value.setText("{}".format(y_slider.value())))
+        y_value.setFont(title)
+        y_value.setStyleSheet('color: deeppink')
+        z_value = QLabel("{}".format(z_slider.value()))
+        z_slider.valueChanged.connect(lambda value: z_value.setText("{}".format(z_slider.value())))
+        z_value.setFont(title)
+        z_value.setStyleSheet('color: deeppink')
+
+        submit_name = QLineEdit('waypoint name')
+        submit_name.setStyleSheet('color: darkgray')
+        submit_name.setFont(unbold)
         submit_start = QPushButton('Source')
         submit_end = QPushButton('Destination')
         verticalSpacer = QSpacerItem(2, 20, QSizePolicy.Minimum)
         horizontalSpacer = QSpacerItem(40, 2, QSizePolicy.Minimum)
 
+
         # Static Controls
         static_controls = QGroupBox("Static Controls")
+        static_controls.setFont(title)
+
+        sc_x_label = QLabel("x:")
+        sc_y_label = QLabel("y:")
+        sc_z_label = QLabel("z:")
+
+        x_static_label = QLabel("x:")
+        y_static_label = QLabel("y:")
+        z_static_label = QLabel("z:")
+
+        x_static = QLineEdit('int')
+        y_static = QLineEdit('int')
+        z_static = QLineEdit('int')
+
+        wp_name_sc = QLineEdit('waypoint name')
+        wp_name_sc.setStyleSheet('color: darkgray')
+        wp_name_sc.setFont(unbold)
+        submit_start_sc = QPushButton('Source')
+        submit_end_sc = QPushButton('Destination')
+        check_joint_limits = QPushButton('Check Joint Limits')
+
+
+
         xyz_pos = QRadioButton("Static Controls")
 
         # Conveyor Controls
@@ -62,12 +120,25 @@ class MainWindow(QWidget):
         acceleration.addWidget(accel_value)
         
 
+        slider_value_x = QHBoxLayout()
+        slider_value_x.addWidget(x_label)
+        slider_value_x.addWidget(x_value)
+
+        slider_value_y = QHBoxLayout()
+        slider_value_y.addWidget(y_label)
+        slider_value_y.addWidget(y_value)
+
+        slider_value_z = QHBoxLayout()
+        slider_value_z.addWidget(z_label)
+        slider_value_z.addWidget(z_value)
+
+
         sliders = QVBoxLayout()
-        sliders.addWidget(x_label)
+        sliders.addLayout(slider_value_x)
         sliders.addWidget(x_slider)
-        sliders.addWidget(y_label)
+        sliders.addLayout(slider_value_y)
         sliders.addWidget(y_slider)
-        sliders.addWidget(z_label)
+        sliders.addLayout(slider_value_z)
         sliders.addWidget(z_slider)
         
         dc_submit = QHBoxLayout()
@@ -82,8 +153,36 @@ class MainWindow(QWidget):
         dc.addLayout(dc_submit)
         dynamic_controls.setLayout(dc)
 
-        sc = QHBoxLayout()
-        sc.addWidget(xyz_pos)
+
+        xyz_header = QHBoxLayout()
+        xyz_header.addWidget(sc_x_label)
+        xyz_header.addWidget(sc_y_label)
+        xyz_header.addWidget(sc_z_label)
+
+        x_entry = QHBoxLayout()
+        x_entry.addWidget(x_static_label)
+        x_entry.addWidget(x_static)
+
+        y_entry = QHBoxLayout()
+        y_entry.addWidget(y_static_label)
+        y_entry.addWidget(y_static)
+
+        z_entry = QHBoxLayout()
+        z_entry.addWidget(z_static_label)
+        z_entry.addWidget(z_static)
+
+        src_dest = QHBoxLayout()
+        src_dest.addWidget(submit_start_sc)
+        src_dest.addWidget(submit_end_sc)
+
+        sc = QVBoxLayout()
+        sc.addLayout(xyz_header)
+        sc.addLayout(x_entry)
+        sc.addLayout(y_entry)
+        sc.addLayout(z_entry)
+        sc.addWidget(check_joint_limits)
+        sc.addWidget(wp_name_sc)
+        sc.addLayout(src_dest)
         static_controls.setLayout(sc)
         
 
